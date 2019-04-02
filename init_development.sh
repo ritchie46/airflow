aa9158e5d7d8df2988447a1d4370f0956a02af2d
ritchie46 <ritchie46@gmail.com>
2019-04-02 11:33:55 +0200

    source env variables for development

--- init_development.sh
+++ init_development.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
 
+(return 0 2>/dev/null) && sourced=1 || sourced=0
+
+if [[ sourced -eq 0 ]]; then
+    echo "Script should be sourced"
+    exit 1
+fi
+
 export AIRFLOW_USERNAME=dev
 export AIRFLOW_PASSWORD=dev

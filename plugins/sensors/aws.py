e49b74f16f97a9d079acbf62d3427946b7705405
ritchie46 <ritchie46@gmail.com>
2019-04-03 13:55:50 +0200

    pull XCom in operator

--- plugins/sensors/aws.py
+++ plugins/sensors/aws.py
@@ -12,14 +12,28 @@ class EmrStepSensor(BaseSensorOperator):
     FAILED_STATE = ['CANCELLED', 'FAILED', 'INTERRUPTED']
 
     @apply_defaults
-    def __init__(self, job_flow_id, step_id, *args, **kwargs):
+    def __init__(self, job_flow_id, step_id, step_id_xcom=None, *args, **kwargs):
+        """
+
+        :param job_flow_id: (str) Job flow id in AWS, (get from XCom)
+        :param step_id: (str) Step id in AWS, (get from XCom)
+        :param step_id_xcom: (str) Name of 'EmrAddStepsOperator' to pull XCom response from. Note:
+                            step_id will be overwritten by this.
+        :param args:
+        :param kwargs:
+        """
         super().__init__(*args, **kwargs, poke_interval=3, soft_fail=False, mode='poke')
         self.emr = boto3.client('emr')
         self.job_flow_id = job_flow_id
         self.step_id = step_id
+        self.step_id_xcom = step_id_xcom
 
     def poke(self, context):
         response = self.emr.describe_step(ClusterId=self.job_flow_id, StepId=self.step_id)
+
+        if self.step_id_xcom is not None:
+            task_instance = context['task_instance']
+            self.step_id = task_instance.xcom_pull(self.step_id_xcom, key='return_value')[-1]
 
         if not response['ResponseMetadata']['HTTPStatusCode'] == 200:
             self.log.info('Bad HTTP response: %s', response)

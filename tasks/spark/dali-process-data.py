cff680929b3f278123f9193e3e13dbfb84c5cf25
Castelijns <Thomas.Castelijns@enexis.nl>
2019-04-10 10:59:52 +0200

    Bugfix

--- tasks/spark/dali-process-data.py
+++ tasks/spark/dali-process-data.py
@@ -13,16 +13,16 @@ conf = SparkConf()
 sc = SparkContext(conf=conf)
 spark = SQLContext(sc)
 
-year = sys.argv[0]
-month = sys.argv[1]
-day = sys.argv[2]
+year = int(sys.argv[0])
+month = int(sys.argv[1])
+day = int(sys.argv[2])
 startdate_update = datetime.datetime(year, month, day, 0, 0) + datetime.timedelta(days=1)
 
 enddate_update = datetime.datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
 
 dates_to_parse = [startdate_update + datetime.timedelta(i) for i in range((enddate_update-startdate_update).days)]
 
-base_path = "s3://enxt-dl-raw/salvador/sensordata/readings/year={}/month={}{}/day={}{}/*"
+base_path = "s3://enxt-dl-raw/salvador/sensordata/readings/year={}/month={}/day={}/*"
 json_paths = [base_path.format(i.strftime('%Y'), i.strftime('%m'), i.strftime('%d')) for i in dates_to_parse]
 
 # print('Load files from:', json_path)

a38c0e3f1419c048eaba3331f8867f4afe567450
ritchie46 <ritchie46@gmail.com>
2019-05-07 10:17:14 +0200

    nat settings tested

--- tasks/development/diagnostics.py
+++ tasks/development/diagnostics.py
@@ -1,9 +1,14 @@
 import platform
 import sys
 import time
+import botocore.vendored.requests as requests
 
 print('Python version:', platform.python_version())
 print('Python script called with the following sysargs:', sys.argv)
+
+response = requests.get('http://ifconfig.me')
+print('Public ip:', response.text)
+
 
 while True:
     time.sleep(60)
